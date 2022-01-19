95823583bcf8fd8230c42d3f16b79a80fea092e7
Rick Mark <rickmark@outlook.com>
2022-01-18 23:20:31 -0800

    Apple SecureROMs

---
+++ Formula/apple-roms.rb
@@ -0,0 +1,13 @@
+# typed: false
+# frozen_string_literal: true
+
+class AppleRoms < Formula
+  desc "Known SecureROM and SEPROM binaries"
+  homepage "https://github.com/hekapooios/hekapooios.github.io"
+  head "https://github.com/hekapooios/hekapooios.github.io.git"
+
+  install do
+    pkgshare.install Dir['resources/SEPROM/*']
+    pkgshare.install Dir['resources/APROM/*']
+  end
+end
