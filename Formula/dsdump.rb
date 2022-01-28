bc9dfdbefd4fdd7f5a9077f643878cf75fc7aceb
Rick Mark <rickmark@outlook.com>
2022-01-28 01:40:51 -0800

    fixes for dsdump

--- Formula/dsdump.rb
+++ Formula/dsdump.rb
@@ -4,14 +4,15 @@
 class Dsdump < Formula
   desc "An improved nm + Objective-C & Swift class-dump"
   homepage "https://github.com/DerekSelander/dsdump"
+  head 'https://github.com/DerekSelander/dsdump.git'
+  url "https://github.com/DerekSelander/dsdump/blob/master/compiled/dsdump_compiled.zip", :using => :curl
+
   version "0.1.0"
-  sha256 "83eebd025b43b58a486235e1bec70a3239995be409605e3ff19bdae07adff917"
-
-
-  url "https://github.com/DerekSelander/dsdump/blob/master/compiled/dsdump.zip", :using => :curl
-
+  sha256 "fe2de7d58048f92735c89686fd8c5bab92b5e8d0cf7ba6dce4ab00e62d0c7529"
 
   def install
+    
+
     bin.install "selander/dsdump"
   end
 end
