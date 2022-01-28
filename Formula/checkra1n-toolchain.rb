64cf75d2df5b3cb514f4cc1325d31d07e9c5e33c
Rick Mark <rickmark@outlook.com>
2022-01-28 01:14:56 -0800

    checkra1n toolchain

--- Formula/checkra1n-toolchain.rb
+++ Formula/checkra1n-toolchain.rb
@@ -7,5 +7,7 @@ class Checkra1nToolchain < Formula
     homepage "https://github.com/checkra1n/toolchain"
     depends_on :macos
 
-    
+    url 'https://github.com/checkra1n/toolchain/archive/refs/tags/v0.1.tar.gz'
+    version "0.1"
+    sha256 'c69dac5da25feb9a0573d4223874b959e233ccae52b854855a30eeea1cc8e1df'
 end
