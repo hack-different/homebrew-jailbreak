70d345fd39c68c22429ceedaa21c3e97f851f2f9
Rick Mark <rickmark@outlook.com>
2022-01-31 23:43:27 -0700

    Update apple-roms.rb

--- Formula/apple-roms.rb
+++ Formula/apple-roms.rb
@@ -1,6 +1,8 @@
 # typed: false
 # frozen_string_literal: true
 
+# we should create JaikbreakFormula that pretends it's not tap only by generating a version in the
+# form of the height of the "main" branch
 class AppleRoms < Formula
   desc "Known SecureROM and SEPROM binaries"
   homepage "https://github.com/hekapooios/hekapooios.github.io"
