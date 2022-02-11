6a968d541d8d3b8222df35f2b3965021ec719f48
Rick Mark <rickmark@outlook.com>
2022-02-11 14:02:19 -0800

    typo

--- Formula/img4lib.rb
+++ Formula/img4lib.rb
@@ -1,10 +1,10 @@
 # typed: false
 # frozen_string_literal: true
-class Img4Lib < Formula
+class Img4lib < Formula
     desc "img4 library and tool"
     homepage "https://github.com/xerub/img4lib"
     version "1.0"
     head "https://github.com/xerub/img4lib.git"
 
-    
+
 end
