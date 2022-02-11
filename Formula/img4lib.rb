c2b60392b69fda247df8e82516b79497f495ad69
Rick Mark <rickmark@outlook.com>
2022-02-11 14:06:45 -0800

    install img4

--- Formula/img4lib.rb
+++ Formula/img4lib.rb
@@ -5,6 +5,9 @@ class Img4lib < Formula
     homepage "https://github.com/xerub/img4lib"
     version "1.0"
     head "https://github.com/xerub/img4lib.git"
+    url "https://github.com/xerub/img4lib/releases/download/1.0/img4lib-2020-10-27.tar.gz"
 
-
+    def install
+        bin.install "img4"
+    end
 end
