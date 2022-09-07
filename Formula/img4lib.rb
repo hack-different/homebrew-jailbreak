36c99f269f8644ee68c0858cc4afe9dd63034cd0
0x8ff <74850058+0x8ff@users.noreply.github.com>
2022-09-06 21:22:58 -0700

    Fix CI? [3]

--- Formula/img4lib.rb
+++ Formula/img4lib.rb
@@ -2,11 +2,11 @@
 # frozen_string_literal: true
 
 class Img4lib < Formula
-  desc "img4 library and tool"
+  desc "Img4 library and tool"
   homepage "https://github.com/xerub/img4lib"
+  url "https://github.com/xerub/img4lib/releases/download/1.0/img4lib-2020-10-27.tar.gz"
   version "1.0"
   head "https://github.com/xerub/img4lib.git"
-  url "https://github.com/xerub/img4lib/releases/download/1.0/img4lib-2020-10-27.tar.gz"
 
   depends_on "openssl"
     
